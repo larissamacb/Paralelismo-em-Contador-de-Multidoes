9cf82b75383816d4f14a6554000f2b8605138509
Larissa Maciel <151437687+larissamacb@users.noreply.github.com>
2025-06-05 12:07:49 +0000

    Reconhecendo imagens de cabeça e arquivo com o funcionamento

--- models/experimental.py
+++ models/experimental.py
@@ -116,6 +116,8 @@ def attempt_load(weights, map_location=None):
     for w in weights if isinstance(weights, list) else [weights]:
         attempt_download(w)
         model.append(torch.load(w, map_location=map_location)['model'].float().fuse().eval())  # load FP32 model
+        #model.append(torch.load(w, map_location=map_location, weights_only=False)['model'].float().fuse().eval())
+
 
     # Compatibility updates
     for m in model.modules():
